5e622ef41a2456e90a8aecc4ca470e131889391c
jjorissen52 <jjorissen52@gmail.com>
2021-09-17 12:04:50 -0500

    demo of tests working when not using esbuild-jest

--- jest.config.js
+++ jest.config.js
@@ -3,15 +3,4 @@ module.exports = {
     testEnvironment: 'jsdom',
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
     moduleDirectories: ['node_modules', '<rootDir>'],
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': [
-        'esbuild-jest',
-        {
-          sourcemap: true,
-          loaders: {
-            '.spec.ts': 'tsx'
-          }
-        }
-      ]
-    }
   };
